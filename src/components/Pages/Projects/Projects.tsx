593a02be3a6840dc1138faf09a4a79e682d39224
KolesnikovAndrew <andrejs.kolesnikovs.tsi@inbox.lv>
2022-01-17 17:20:01 +0200

    Fixed projects description

--- src/components/Pages/Projects/Projects.tsx
+++ src/components/Pages/Projects/Projects.tsx
@@ -12,7 +12,7 @@ export function Projects() {
       <h1>Projects</h1>
       <Project image={DemoSoc} description='This is a demo version of the social network. It is my first project written in React. For the API I used social-network.samuraijs.com free version. For state management I used Redux.' name="Demo Social Network" techs='React JS, Redux, CSS, HTML, JS'/>
       <Project image={FormValidation} description='The user is obliged to fill in the field with the correct e-mail and check the box that he agrees with the tos. Otherwise, the user receives an error message. If the form is filled in correctly, the user receives a message about the successful completion of the form. ' name="Validation of the form" techs='React JS, CSS, HTML, React Hook Form'/>
-      <Project image={FetchTable} description='The front-end makes an axios request to the nodejs API, which is connected to the Postgresql database and receives data, which is then displayed in the table. ' name="Table with fetched from API data" techs='React JS, Axios, Node JS, SCSS, HTML, PostgreSQL'/>
+      <Project image={FetchTable} description='The front-end makes an axios request to the nodejs API, which is connected to the Postgresql database and receives data, which is then displayed in the table. ' name="Table with fetched from API data" techs='Axios, Node JS, SCSS, HTML, PostgreSQL'/>
       
       
     </div>
